eef5b7637dcc753ee86ce7a1acf451751d82db4f
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-24 13:36:59 +0900

    2022 11 24 - ArticleController(list, show)

--- src/main/java/com/mustache/article/controller/ArticleController.java
+++ src/main/java/com/mustache/article/controller/ArticleController.java
@@ -5,8 +5,14 @@ import com.mustache.article.domain.dto.ArticleDto;
 import com.mustache.article.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/articles")
@@ -19,7 +25,30 @@ public class ArticleController {
         this.articleRepository = articleRepository;
     }
 
-    @PostMapping("") // create post
+    @GetMapping("/{id}")
+    public String selectSingle(@PathVariable Long id, Model model) {
+        Optional<Article> optArticle = articleRepository.findById(id);
+        if (!optArticle.isEmpty()) { // 비어있지 않으면
+            model.addAttribute("aritlce", optArticle.get());
+            return "show";
+        } else {
+            return "error";
+        }
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Article> articles = articleRepository.findAll();
+        model.addAttribute("articles", articles);
+        return "list";
+    }
+
+    @GetMapping("")
+    public String index() {
+        return "redirect:/articles/list";
+    }
+
+    @PostMapping("/new") // create post
     public String add(ArticleDto articleDto) {
         log.info(articleDto.getTitle()); // title을 log로 기록
         Article saveArticle = articleRepository.save(articleDto.toEntity());
